a9f600beff7b73a76b261611675ae5e0c2f229ed
Brun0 <brunodev41@gmail.com>
2025-04-01 13:58:44 -0300

    Add Header component to App and remove old Header implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Header from "./assets/components/header/Header";
 
 const App = () => {
-  return <div></div>;
+  return <div>
+
+    <Header />
+  </div>;
 };
 
 export default App;
