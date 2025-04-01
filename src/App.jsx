57e9aad70a8fee1ade31213c53c4b4a081c7361e
Brun0 <brunodev41@gmail.com>
2025-04-01 11:40:46 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+const App = () => {
+  return <div className="text-bg-danger">App</div>;
+};
+
+export default App;
